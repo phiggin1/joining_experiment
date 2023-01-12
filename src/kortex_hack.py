a5ce57261fea5a7d48bbf4510ca232bb6db7f210
phiggin1 <phiggin1@umbc.edu>
2023-01-12 16:05:35 -0500

    added in intermediate node for physical arm for cart vel control

--- src/kortex_hack.py
+++ src/kortex_hack.py
@@ -1,28 +1,46 @@
 #!/usr/bin/env python3
 
 import rospy
-from kortex_driver.msg import Base_JointSpeeds, JointSpeed
-from trajectory_msgs.msg import JointTrajectory
+from kortex_driver.msg import TwistCommand, Twist
+from geometry_msgs.msg import TwistStamped, PoseStamped
 
 class KortexHack:
     def __init__(self):
         rospy.init_node('kortex_hacked_fix', anonymous=True)
-        self.servo_sub = rospy.Subscriber('/my_gen3/gen3_joint_trajectory_controller/command', JointTrajectory, self.servo_cb)
-        self.joint_vel_pub = rospy.Publisher('/my_gen3/in/joint_velocity', Base_JointSpeeds, queue_size=10)
+        self.safe = False
+        self.servo_sub = rospy.Subscriber('/my_gen3/servo_server/delta_twist_cmds', TwistStamped, self.delta_twist_cmds_cb)
+        self.cart_vel_pub = rospy.Publisher('/my_gen3/in/cartesian_velocity', TwistCommand, queue_size=10)
+        self.finger_sub = rospy.Subscriber('finger_pose', PoseStamped, self.get_finger_pose)
+
         rospy.spin()
 
-    def servo_cb(self, joint_traj):
-        point = joint_traj.points[0]
+    def get_finger_pose(self, pose):
+        if (pose.pose.position.z < 0.1):
+            self.safe = False
+        else:
+            self.safe = True
 
-        joint_speeds = Base_JointSpeeds()
-        for i, v in enumerate(point.velocity):
-            speed = JointSpeed()
-            speed.joint_identifier = i
-            speed.value = v
-            speed.duration = 0
-            joint_speeds.joint_speeds.append(speed)
+    def delta_twist_cmds_cb(self, delta_twist):
+        twist = TwistCommand()
+        twist.reference_frame = 0
+        twist.duration = 0
 
-        self.joint_vel_pub.publish(joint_speeds)
+        if self.safe:
+            twist.twist.linear_x = delta_twist.twist.linear.x
+            twist.twist.linear_y = delta_twist.twist.linear.y
+            twist.twist.linear_z = delta_twist.twist.linear.z
+            twist.twist.angular_x = delta_twist.twist.angular.x
+            twist.twist.angular_y = delta_twist.twist.angular.y
+            twist.twist.angular_z = delta_twist.twist.angular.z
+        else:
+            twist.twist.linear_x = 0.0
+            twist.twist.linear_y = 0.0
+            twist.twist.linear_z = 0.0
+            twist.twist.angular_x = 0.0
+            twist.twist.angular_y = 0.0
+            twist.twist.angular_z = 0.0
+
+        self.cart_vel_pub.publish(twist)
 
 if __name__ == '__main__':
-    track = KortexHack()
+    physical_arm = KortexHack()
